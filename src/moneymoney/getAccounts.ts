aa1e47c8337f3d28f638b36da28673f4e0486a8c
Xiphe <github@xiphe.net>
2020-04-12 01:52:36 +0200

    feat: ensure selectable accounts can hande transactions
    fix https://github.com/Xiphe/budgetbudget/issues/17

--- src/moneymoney/getAccounts.ts
+++ src/moneymoney/getAccounts.ts
@@ -1,6 +1,19 @@
 import { Account, validateAccount } from './Types';
 import { ipcRenderer } from '../lib';
 import { parse } from 'plist';
+import getTransactions from './getTransactions';
+
+async function accountHandlesTransactions(
+  accountNumber: string,
+  currency: string,
+): Promise<boolean> {
+  try {
+    await getTransactions([accountNumber], currency, new Date().getTime());
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
 
 export default async function getAccounts(
   currency: string,
@@ -11,21 +24,31 @@ export default async function getAccounts(
     throw new Error('Unexpectedly got non-array as accounts');
   }
 
-  return parsedStdout
-    .map((data: unknown): Account | false => {
-      const { accountNumber, balance, name } = validateAccount(data);
-      if (!accountNumber.length) {
-        return false;
-      }
-      const currencyBalance = balance.find(([_, c]) => c === currency);
-      if (!currencyBalance) {
-        return false;
-      }
-      return {
-        name,
-        number: accountNumber,
-        balance: currencyBalance[0],
-      };
-    })
-    .filter((data: Account | false): data is Account => Boolean(data));
+  const accounts = await Promise.all(
+    parsedStdout.map(
+      async (data: unknown): Promise<Account | false> => {
+        const { accountNumber, balance, name } = validateAccount(data);
+        if (!accountNumber.length) {
+          return false;
+        }
+        const currencyBalance = balance.find(([_, c]) => c === currency);
+        if (
+          !currencyBalance ||
+          !(await accountHandlesTransactions(accountNumber, currency))
+        ) {
+          return false;
+        }
+
+        return {
+          name,
+          number: accountNumber,
+          balance: currencyBalance[0],
+        };
+      },
+    ),
+  );
+
+  return accounts.filter((data: Account | false): data is Account =>
+    Boolean(data),
+  );
 }
