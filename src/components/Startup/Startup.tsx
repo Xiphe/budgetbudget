2ce8194a830ca16a97a44bd6c22a72dd4dc82049
Xiphe <github@xiphe.net>
2020-04-30 22:33:14 +0200

    chore: remove leftovers from startup experiment

--- src/components/Startup/Startup.tsx
+++ src/components/Startup/Startup.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import Content from '../Content';
 import Header, { HeaderSpacer } from '../Header';
 import Loading from '../Loading';
-import styles from './Startup.module.scss';
 
 export default function Startup() {
   return (
     <Content
-      className={styles.startup}
-      background
       header={
         <Header center>
           <HeaderSpacer />
