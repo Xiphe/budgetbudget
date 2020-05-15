f40486e6ff5aae562f5794bdb9ea44a50f0bf826
Xiphe <github@xiphe.net>
2020-05-15 22:19:28 +0200

    perf: load initialInitRes in bootstrap
    also removing global deps of app

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,41 @@
 import 'focus-visible';
 import initTheme from './initTheme';
+import { InitRes } from './budget/getInitData';
 
-(process.env.REACT_APP_ENV === 'test'
-  ? /* Delay app startup until test environment calls startApp */
-    import('./__mocks__').then(({ default: expose }) => {
-      return new Promise((res) => expose('_startApp', res));
-    })
-  : /* Non-ticking Promise.resolve() */
-    { then: (cb: () => any) => cb() }
-)
-  .then(() => {
-    initTheme();
+async function getInitialInitResource() {
+  return (await import('./budget/getInitData')).default();
+}
 
-    return Promise.all([
-      import('react'),
-      import('react-dom'),
-      import('./App'),
-    ] as any);
-  })
-  .then(
-    ([{ createElement }, { unstable_createRoot }, { default: App }]: any) => {
-      unstable_createRoot(document.getElementById('root')).render(
-        createElement(App),
-      );
-    },
-  );
+const appEntry = document.getElementById('root')!;
+(async () => {
+  if (process.env.REACT_APP_ENV === 'test') {
+    const { default: expose } = await import('./__mocks__');
+    await new Promise((res) => expose('_startApp', res));
+  }
+
+  initTheme();
+
+  const [
+    initialInitRes,
+    { createElement },
+    { unstable_createRoot },
+    { default: App },
+  ] = await Promise.all<
+    InitRes,
+    typeof import('react'),
+    any, // TODO: use real type once concurrent mode is stable typeof import('react-dom'),
+    typeof import('./App')
+  >([
+    getInitialInitResource(),
+    import('react'),
+    import('react-dom'),
+    import('./App'),
+  ]);
+
+  unstable_createRoot(appEntry).render(createElement(App, { initialInitRes }));
+})().catch((err) => {
+  appEntry.innerHTML = `<div>
+      <h1>Really really really unexpected Error:</h1>
+      <p>${err.message}</p>
+    </div>`;
+});
