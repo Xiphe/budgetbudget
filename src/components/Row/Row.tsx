07520cf8c9143d086bcb50eb557e38d952138e5c
Xiphe <github@xiphe.net>
2020-09-23 20:15:52 +0200

    fix(BudgetView): improve accessibility of category table

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,25 +1,26 @@
-import React, { ReactNode } from 'react';
+import React, { HTMLAttributes } from 'react';
 import classNames from 'classnames';
 import styles from './Row.module.scss';
 
-type Props = {
+type Props = Omit<HTMLAttributes<HTMLDivElement>, 'style'> & {
   indent?: number;
   leaf?: boolean;
   odd?: boolean;
-  children?: ReactNode;
+  'data-row'?: never;
   groupClosed?: boolean;
-  className?: string;
 };
 export default function Row({
   indent = 0,
   leaf = false,
-  children,
   odd,
   groupClosed,
   className,
+  ...rest
 }: Props) {
   return (
     <div
+      role="row"
+      {...rest}
       data-row={leaf ? 'leaf' : 'group'}
       style={{ '--indent': indent } as any}
       className={classNames(
@@ -29,8 +30,6 @@ export default function Row({
         odd && styles.odd,
         groupClosed && styles.groupClosed,
       )}
-    >
-      {children}
-    </div>
+    />
   );
 }
--- src/views/Month/Categories.tsx
+++ src/views/Month/Categories.tsx
@@ -23,6 +23,7 @@ type BudgetRowProps = BudgetCategoryRow & {
 function BudgetRow({
   numberFormatter,
   groupClosed,
+  name,
   odd,
   uuid,
   overspendRollover,
@@ -62,9 +63,14 @@ function BudgetRow({
       className={styles.budgetRow}
       indent={indentation}
       leaf={true}
+      aria-label={name}
       groupClosed={groupClosed}
     >
-      <span className={classNames(budgeted === 0 && styles.zero)}>
+      <span
+        role="gridcell"
+        aria-label="budgeted"
+        className={classNames(budgeted === 0 && styles.zero)}
+      >
         <BudgetInput
           onChange={setBudgeted}
           value={budgeted}
@@ -72,10 +78,16 @@ function BudgetRow({
           numberFormatter={numberFormatter}
         />
       </span>
-      <span className={classNames(spend === 0 && styles.zero)}>
+      <span
+        role="gridcell"
+        aria-label="spend"
+        className={classNames(spend === 0 && styles.zero)}
+      >
         {format(spend)}
       </span>
       <span
+        role="gridcell"
+        aria-label="balance"
         onContextMenu={showContextMenu}
         className={classNames(
           balance === 0 && styles.zero,
@@ -96,28 +108,40 @@ export default function Categories({
 }: CategoriesProps) {
   const { format } = numberFormatter;
   return (
-    <>
+    <div role="grid">
       {mapCategories(
         budgetCategories,
         collapsedCategories,
         (entry, i, groupClosed) => {
           if (entry.group) {
-            const { uuid, indentation, budgeted, spend, balance } = entry;
+            const { uuid, indentation, budgeted, spend, balance, name } = entry;
             return (
               <Row
                 odd={!(i % 2)}
+                role="rowheader"
+                aria-label={name}
                 key={uuid}
                 className={classNames(styles.budgetRow, styles.budgetRowGroup)}
                 indent={indentation}
                 groupClosed={groupClosed}
               >
-                <span className={classNames(budgeted === 0 && styles.zero)}>
+                <span
+                  role="columnheader"
+                  aria-label="budgeted"
+                  className={classNames(budgeted === 0 && styles.zero)}
+                >
                   {format(budgeted)}
                 </span>
-                <span className={classNames(spend === 0 && styles.zero)}>
+                <span
+                  role="columnheader"
+                  aria-label="spend"
+                  className={classNames(spend === 0 && styles.zero)}
+                >
                   {format(spend)}
                 </span>
                 <span
+                  role="columnheader"
+                  aria-label="balance"
                   className={classNames(
                     balance === 0 && styles.zero,
                     balance < 0 && styles.negativeBalance,
@@ -141,6 +165,6 @@ export default function Categories({
           );
         },
       )}
-    </>
+    </div>
   );
 }
